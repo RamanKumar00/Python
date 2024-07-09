ed5747a9908faacd034c6da2c107ed9cec96962e
RamanKumar00 <ramankr7321@gmail.com>
2024-07-09 08:53:53 +0530

    string basic in basic

---
+++ accessing_string.py
@@ -0,0 +1,13 @@
+name="Harry"
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+
+apple='''hii
+        i m Ram 
+        i m learning coding'''
+    
+for character in apple:
+    print(character)
---
+++ stringBasics.py
@@ -0,0 +1,6 @@
+a='ram'
+b="Ram"
+c='''RAM'''
+print(a)
+print(b)
+print(c)
