c59a7b0e63f8e666a3685072b84a04ab259bc868
RamanKumar00 <ramankr7321@gmail.com>
2024-07-13 15:27:11 +0530

    for loop and range function.

---
+++ for_loop.py
@@ -0,0 +1,9 @@
+fruits=["apple","banana","grapes","guava"]
+for x in fruits:
+    if x=="grapes" :
+        break
+    print(x)
+    
+name="lifeGood"     #to give space between words
+for x in name:
+    print(x,end=" ")
---
+++ range_function.py
@@ -0,0 +1,2 @@
+for x in range(6,20,2):   #last element is for increment and 20 is not including in this
+    print (x)
