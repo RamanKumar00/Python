4a3ab4914c4f7951fc212c9198ff4aa902bd7071
RamanKumar00 <ramankr7321@gmail.com>
2025-02-09 12:10:59 +0530

    list full concept

---
+++ list.py
@@ -0,0 +1,12 @@
+ls = [1,2,3,4,"raman",'true']
+
+print(ls[1])
+print(ls[-3]) #negative index -- just count same number from the last of the list
+
+if 3 in ls:
+    print("yes")
+else:
+    print("no")
+
+if "man" in "raman":
+    print("yes")
