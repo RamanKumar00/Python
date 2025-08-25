58a62e9d08ca8d647e09b4ce5a7d19f6806d5ce3
Raman Kumar <ramankr7321@gmail.com>
2025-08-25 18:05:12 +0530

    playing with numbers

---
+++ Number.py
@@ -0,0 +1,14 @@
+#we can do various things in number 
+#type conversion
+num = 5.6
+print(int(num)) 
+
+print(complex(num))
+
+#to print random number 
+import random
+print(random.random())
+
+from decimal import Decimal
+
+print(Decimal('0.5') +Decimal('0.5') + Decimal('0.5'))
