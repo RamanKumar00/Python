fd22ef842746d7f9d928b91d78ff69e6c5c7164a
Raman Kumar <ramankr7321@gmail.com>
2025-09-21 18:22:34 +0530

    understanding lists

--- String.py
+++ String.py
@@ -1,12 +1,34 @@
-username = "ChaiaurCode"
-print(username)
+# username = "ChaiaurCode"
+# print(username)
 
-print(username[0])  #it prints the first index of the string
+# print(username[0])  #it prints the first index of the string
 
-print(username[-1]) # it prints the last index of the character 
+# print(username[-1]) # it prints the last index of the character 
 
-print(len(username)) # it give the length of the username 
+# print(len(username)) # it give the length of the username 
 
-print(username[1:3]) # it print the slicing and it include the first index and exclude the last index  and it is also known as slicing of the string
+# print(username[1:3]) # it print the slicing and it include the first index and exclude the last index  and it is also known as slicing of the string
 
-print(dir(username)) # dir show that which  function we perform for a string   
+# print(dir(username)) # dir show that which  function we perform for a string   
+
+ 
+# print(num_List[:])
+
+# print(num_List[0:6])
+# print(num_List[-1])
+# print(num_List[0:7:3])
+
+# chai = "Masala Chai"
+# print(chai.lower())
+# print(chai.upper())
+
+# chips = "   Kurkure"
+# print(chips.strip())
+
+# print(chai.replace("Masala","Ginger"))
+
+# types = "Lemon, Ginger, Masala, Mint"
+# print(types.split(", "))
+num_List = "0123456789"
+print(len(num_List))
+print(num_List[-1])
--- list.py
+++ list.py
@@ -10,3 +10,16 @@ myList[0] = "Virat"
 
 print(myList[0] )
 
+tea_varities = ["lemon","oolong","black","red"]
+print(tea_varities)
+
+print(tea_varities[0])
+
+tea_varities[3] = "Milk Tea"
+print(tea_varities)
+
+tea_varities[1:3] = ["Masala", "green"]
+print(tea_varities)
+
+for tea in tea_varities:
+    print(tea, end="-")
