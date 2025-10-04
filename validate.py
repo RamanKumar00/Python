58204d0a71e0bfd79c573ca254f465a05ebf52cb
Raman Kumar <ramankr7321@gmail.com>
2025-10-04 18:20:41 +0530

    solving problem on loop

---
+++ validate.py
@@ -0,0 +1,8 @@
+while True:
+    number = int(input("enter a number b/w 1 to 10: "))
+    if 1 <= number <=10:
+        print("thanks")
+        break
+    
+    else:
+        print("invalid number, try again")
